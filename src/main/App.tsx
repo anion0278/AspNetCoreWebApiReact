099a90798d355eb8b55c161ac30459874622b664
Stefan Grushko <anion0278@gmail.com>
2024-08-31 19:17:28 +0200

    Consuming enpoint from react houseList component

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,11 +1,12 @@
+import HouseList from '../house/HouseList'
 import './App.css'
 import Header from './Header'
 
 function App() {
   return (
     <div className='container'>
-
       <Header subtitle='List of houses on sale' />
+      <HouseList />
     </div>
   )
 }
