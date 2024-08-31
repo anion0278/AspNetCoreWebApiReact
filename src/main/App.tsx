864ac104e2e1f90f16a82d43ac6a02b5a37fbe76
Stefan Grushko <anion0278@gmail.com>
2024-08-31 17:48:28 +0200

    Initial react app

---
+++ src/main/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className='container'>
+
+      <Header subtitle='List of houses on sale' />
+    </div>
+  )
+}
+
+export default App
