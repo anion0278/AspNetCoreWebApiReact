3186bdc3fa0d1408ade3021f57648daca4056a13
Stefan Grushko <anion0278@gmail.com>
2024-08-31 18:33:47 +0200

    Add simple houses endpoint

--- api/Data/HouseDbContext.cs
+++ api/Data/HouseDbContext.cs
@@ -2,8 +2,10 @@ using Microsoft.EntityFrameworkCore;
 
 public class HouseDbContext : DbContext
 {
-
     public DbSet<HouseEntity> Houses => Set<HouseEntity>();
+
+    public HouseDbContext(DbContextOptions<HouseDbContext> options): 
+        base(options)   {}
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -11,6 +13,7 @@ public class HouseDbContext : DbContext
         var path = Environment.GetFolderPath(folder);
 
         optionsBuilder.UseSqlite($"Data Source={Path.Join(path, "houses2.db")}");
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
