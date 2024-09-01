586f6891079badd1be594a0bae1fd775a5de2590
Stefan Grushko <anion0278@gmail.com>
2024-09-01 17:04:00 +0200

    Add endpoints for bids

--- api/Program.cs
+++ api/Program.cs
@@ -27,48 +27,7 @@ app.UseCors(
 
 app.UseHttpsRedirection();
 
-app.MapGet("/houses", async (IHouseRepository houseRepository) =>
-    await houseRepository.GetAll()).Produces<HouseDto[]>(StatusCodes.Status200OK);
-
-app.MapGet("/house/{houseId:int}", async (int houseId, IHouseRepository repository) =>
-{
-    var house = await repository.Get(houseId);
-    if (house is null) return Results.Problem($"Could not find house with ID {houseId}", statusCode: 404);
-    return Results.Ok(house);
-}).ProducesProblem(404).Produces<HouseDetailsDto>(StatusCodes.Status200OK);
-
-app.MapPost("/houses", async ([FromBody] HouseDetailsDto dto, IHouseRepository repository) =>
-{
-    if (!MiniValidator.TryValidate(dto, out var errors))
-        return Results.ValidationProblem(errors);
-    var newHouse = repository.Add(dto);
-    return Results.Created($"/house/{newHouse.Id}", newHouse);
-}).Produces<HouseDetailsDto>(StatusCodes.Status201Created)
-.ProducesValidationProblem();
-
-app.MapPut("/houses/", async ([FromBody] HouseDetailsDto dto, IHouseRepository repository) =>
-{
-    if (!MiniValidator.TryValidate(dto, out var errors))
-        return Results.ValidationProblem(errors);
-    if (await repository.Get(dto.Id) is null)
-    {
-        return Results.Problem($"House wit ID {dto.Id} not found", statusCode: 404);
-    }
-    var updatedHouse = await repository.Update(dto);
-    return Results.Ok(dto);
-}).ProducesProblem(404)
-.Produces<HouseDetailsDto>(StatusCodes.Status200OK)
-.ProducesValidationProblem();
-
-app.MapDelete("/houses/{houseId:int}", async (int houseId, IHouseRepository repository) =>
-{
-    if (await repository.Get(houseId) is null)
-    {
-        return Results.Problem($"House wit ID {houseId} not found", statusCode: 404);
-    }
-    await repository.Delete(houseId);
-    return Results.Ok();
-}).ProducesProblem(404).Produces<HouseDetailsDto>(StatusCodes.Status200OK);
-
+app.MapHouseEndpoints();
+app.MapBidEndpoints();
 
 app.Run();
---
+++ api/WebApplicationBidExtensions.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using MiniValidation;
+
+public static class WebApplicationBidExtensions
+{
+    public static void MapBidEndpoints(this WebApplication app)
+    {
+        app.MapGet("/house/{houseId:int}/bids", async (int houseId, IBidRepository bidRepository, IHouseRepository houseRepository) =>
+        {
+            if (await houseRepository.Get(houseId) is null) return Results.Problem($"House wit ID {houseId} not found", statusCode: 404);
+            var bids = await bidRepository.Get(houseId);
+            return Results.Ok(bids);
+        }).ProducesProblem(404)
+        .Produces<BidDto[]>(StatusCodes.Status200OK);
+
+        app.MapPost("/house/{houseId:int}/bids", async (int houseId, [FromBody] BidDto bid, IBidRepository bidRepository, IHouseRepository houseRepository) =>
+        {
+            if (bid.HouseId != houseId) return Results.Problem($"House wit ID {houseId} not found", statusCode: StatusCodes.Status400BadRequest);
+
+            if (!MiniValidator.TryValidate(bid, out var errors))
+                return Results.ValidationProblem(errors);
+
+            if (await houseRepository.Get(houseId) is null) return Results.Problem($"House wit ID {houseId} not found", statusCode: 404);
+
+            var bidCreated = await bidRepository.Add(bid);
+            return Results.Created($"/house/{bidCreated.HouseId}/bids", bidCreated);
+        }).ProducesProblem(404)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .Produces<BidDto>(StatusCodes.Status200OK);
+    }
+}
