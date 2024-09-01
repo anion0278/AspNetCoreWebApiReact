59bb87bfc0a6a62162b44e269c8d70fefb7cc052
Stefan Grushko <anion0278@gmail.com>
2024-09-01 09:41:14 +0200

    endpoint for single house retrieval

--- api/Data/HouseRepository.cs
+++ api/Data/HouseRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public interface IHouseRepository
 {
     Task<List<HouseDto>> GetAll();
+    Task<HouseDetailsDto?> Get(int id);
 }
 
 
@@ -21,4 +22,12 @@ public class HouseRepository: IHouseRepository
                 .Select(he => new HouseDto(he.Id, he.Address, he.Country, he.Price))
                 .ToListAsync();
     }
+
+    public async Task<HouseDetailsDto?> Get(int id)
+    {
+        var h = await _dbContext.Houses.SingleOrDefaultAsync(h => h.Id == id);
+        if (h is null) return null;
+        return new HouseDetailsDto(h.Id, h.Address, h.Country, h.Price, h.Description, h.Photo);
+    }
+
 }
---
+++ api/Dtos/HouseDetailDto.cs
@@ -0,0 +1,7 @@
+public record HouseDetailsDto( 
+    int Id, 
+    string? Address, 
+    string? Country, 
+    decimal Price, 
+    string? Description, 
+    string? Photo);
